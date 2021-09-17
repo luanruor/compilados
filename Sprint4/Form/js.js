3d180eacb5abe8f084289fae71e1e7006860712f
luanruor <luanruor@hotmail.com>
2021-09-17 07:56:58 -0500

    Update js.js

--- Sprint4/Form/js.js
+++ Sprint4/Form/js.js
@@ -1,34 +1,24 @@
-const crearDiv = () => {
-
+const crearDiv=()=>{
     const numDivs = document.getElementsByClassName("item").length;
-    
     if(numDivs < 10){
-    
-      const contenedor = document.getElementById("contenedor-divs");
-      let id = idDiv();
-      let nuevoDiv = `<div id="`+id+`" class="item">
-                        Este es el DIV con el id <strong>`+id +`</strong> <input type="button" class="eliminar" onClick="eliminarDiv(this)" value="Eliminar este DIV">
-                      </div>`;
-      
-      contenedor.insertAdjacentHTML("beforeend", nuevoDiv);
-      
+        const contenedor = document.getElementById("contenedor-divs");
+        let id = idDiv();
+        let nuevoDiv = '<div id="'+id+'" class="item">Este es el DIV con el id <strong>'+id +'</strong> <input type="button" class="eliminar" onClick="eliminarDiv(this)" value="Eliminar este DIV"></div>';
+        contenedor.insertAdjacentHTML("beforeend", nuevoDiv);
     }else{
-      alert('Solo puedes agregar 10 DIVs, si deseas agregar más lo puedes hacer modificando el codigo.');
+        alert('Solo puedes agregar 10 DIVs, si deseas agregar más lo puedes hacer modificando el codigo.');
     }
-  
-  }
-  
-  const idDiv = () => {
-    
+}
+
+const idDiv = () => {
     for(var i = 1; i <= 10; i++){
-      if(document.getElementById("div"+i) === null){
-        return "div"+i;
-      }
+        if(document.getElementById("div"+i) === null){
+            return "div"+i;
+        }
     }
-  
-  }
-  
-  const eliminarDiv = (obj) => {
+}
+
+const eliminarDiv=(obj)=>{
     const div = obj.parentElement;
     div.remove();
-  }
+}
